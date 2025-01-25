22606593ad296d0262115b91336406c393bd51a1
Tomas <tomas@tomasps.com>
2025-01-25 03:34:00 +0100

    refactor(matrix-server): update routing and configuration for Matrix server

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,13 @@ import { RouterModule, Routes } from "@angular/router";
 import { HomeComponent } from "@components/home/home.component";
 import { PrivacyPolicyComponent } from "@components/privacy-policy/privacy-policy.component";
 import { TermsOfServiceComponent } from "@components/terms-of-service/terms-of-service.component";
-import { MatrixServerComponent } from './components/matrix-server/matrix-server.component';
 
 const routes: Routes = [
   { path: "", component: HomeComponent },
   { path: "privacy-policy", component: PrivacyPolicyComponent },
   { path: "terms-of-service", component: TermsOfServiceComponent },
-  { path: ".well-known/matrix/server", component: MatrixServerComponent },
-  // { path: "**", redirectTo: "" },
+  { path: ".well-known/matrix/server", redirectTo: "assets/.well-known/matrix/server" },
+  { path: "**", redirectTo: "" },
 ];
 
 @NgModule({
